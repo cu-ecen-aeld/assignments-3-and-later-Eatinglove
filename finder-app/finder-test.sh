c639302c219061e120ccfab6b708b0a10c809249
Eatinglove <djes1000303@gmail.com>
2025-04-08 17:05:52 +0000

    monday left me broken

--- finder-app/finder-test.sh
+++ finder-app/finder-test.sh
@@ -32,7 +32,7 @@ echo "Writing ${NUMFILES} files containing string ${WRITESTR} to ${WRITEDIR}"
 rm -rf "${WRITEDIR}"
 
 # create $WRITEDIR if not assignment1
-assignment=`cat ../conf/assignment.txt`
+assignment=`cat conf/assignment.txt`
 
 if [ $assignment != 'assignment1' ]
 then
@@ -51,16 +51,43 @@ fi
 echo "Removing the old writer utility and compiling as a native application"
 #make clean
 #make
+WRITER_PATH=$(realpath ./writer 2>/dev/null || which writer 2>/dev/null)
+if [ -z "$WRITER_PATH" ]; then
+    echo "Error: writer not found!"
+    exit 1
+else
+    echo "Writer program path: $WRITER_PATH"
+fi
+
+for i in $(seq 1 $NUMFILES)
+do
+    FILE_PATH="$WRITEDIR/${username}$i.txt"
+    ./writer "$FILE_PATH" "$WRITESTR"
+    echo "File created: $FILE_PATH"
+done
 
 for i in $( seq 1 $NUMFILES)
 do
 	./writer "$WRITEDIR/${username}$i.txt" "$WRITESTR"
 done
+echo "Current directory: $(pwd)"
+ls
 
-OUTPUTSTRING=$(./finder.sh "$WRITEDIR" "$WRITESTR")
+#OUTPUTSTRING=$(./finder.sh "$WRITEDIR" "$WRITESTR")
+echo "Checking for finder.sh at: $(pwd)/finder.sh"
+ls -l $(pwd)/finder.sh
+FINDER_PATH="/tmp/aesd-autograder/rootfs/home/finder.sh"
+echo "Finder script path: $FINDER_PATH"
+
+if [ -f "$FINDER_PATH" ]; then
+    OUTPUTSTRING=$(bash "$FINDER_PATH" "$WRITEDIR" "$WRITESTR")
+else
+    echo "Error: finder.sh not found at expected path: $FINDER_PATH"
+    exit 1
+fi
 
 # remove temporary directories
-rm -rf /tmp/aeld-data
+#rm -rf /tmp/aeld-data
 
 set +e
 echo ${OUTPUTSTRING} | grep "${MATCHSTR}"
